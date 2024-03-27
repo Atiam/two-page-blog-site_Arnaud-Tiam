34074fd52b41c98ae9885a2041571aac340ff16c
Arnaud Tiam <arnaudtiam50@gmail.com>
2024-03-26 20:22:21 -0500

    Update style.css, blog.js, logic.js and blog.html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,2 +1,22 @@
+let main  = document.getElementById("blogs");
 
+let data = JSON.parse(localStorage.getItem('user')) || [];
+
+for (let i = 0; i < data.length; i++){
+    //create elements for section, h2, p, p
+    const section = document.createElement("section");
+    const title = document.createElement("h2");
+    const content = document.createElement("p");
+    const author = document.createElement("p");
+    // then add the data to each appropriate element
+
+    title.textContent = data[i].title;
+    content.textContent = data[i].content;
+    author.textContent = "posted by: " + data[i].username;
+
+    // then append the h2 and the two p tags into the section
+    section.append(title, content, author);
+    // thenappend the section to the main container
+    main.append(section);
+}
   
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,34 +1,38 @@
-const usernameInput = document.querySelector('#username');
-const titleInput = document.querySelector('#title');
-const contentInput = document.querySelector('#content');
-const sumitButton = document.querySelector('#sumit');
+const usernameInput = document.querySelector("#username");
+const titleInput = document.querySelector("#title");
+const contentInput = document.querySelector("#content");
+const sumitButton = document.querySelector("#sumit");
 
+sumitButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
+  let userintry = JSON.parse(localStorage.getItem('user')) || [];
 
-// sumitButton.addEventListener('click', function (event) {
-//     event.preventDefault();
-//      window.location.href = "blog.html";
-  // create user object from submission
-const user = {
+  const user = {
     username: usernameInput.value.trim(),
     title: titleInput.value.trim(),
-    content: contentInput.value.trim()
+    content: contentInput.value.trim(),
   };
 
-   // set new submission to local storage
-   localStorage.setItem('user', JSON.stringify(user));
-  
+  userintry.push(user);
+
+  // set new submission to local storage
+  localStorage.setItem("user", JSON.stringify(userintry));
+
+  window.location.href = "blog.html";
+});
+
 //add even handler on the submit button to prevent the default behavior , which is reloading the page.
 
-usernameInput.value != null
-titleInput.value != null
-contentInput.value != null
+// usernameInput.value != null
+// titleInput.value != null
+// contentInput.value != null
 
- if (usernameInput.value && titleInput.value && contentInput.value) {
-    sumitButton.addEventListener('click', function (event) {
-        event.preventDefault();
-         window.location.href = "blog.html";
-     })
+//  if (usernameInput.value && titleInput.value && contentInput.value) {
+//     sumitButton.addEventListener('click', function (event) {
+//         event.preventDefault();
+//          window.location.href = "blog.html";
+//      })
 
-else {alert("please complete the form")};
- }
+//     }
+//     else {alert("please complete the form")};
